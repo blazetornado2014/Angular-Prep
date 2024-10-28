d495e676a948b5e4290df1760aa89f5ceaa94f2f
blazetornado2014 <155018915+blazetornado2014@users.noreply.github.com>
2024-10-28 10:38:59 -0700

    Updating base code
    Base Code added

---
+++ angularProject/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angularProject';
+  a = 10;
+  b = 5;
+  ans = this.a + this.b;
+
+  imgSrc = '/assets/Fink_Profile.png'
+  display(msg:string){
+    alert(msg)
+  }
+
+  isShift(event:any){
+    if (event.shiftKey && event.key === 'Y'){
+      console.log("Shift + Y is pressed", event);
+    }
+  }
+
+  inputVal:string = '';
+  dynamicVal:string = '';
+  show(){
+    this.dynamicVal = this.inputVal;
+  }
+
+  count = 0;
+  counter(type:string){
+    type==='incr'? this.count++ : this.count--;
+  }
+
+  displayElement:boolean = false;
+  showNgIf(){
+    this.displayElement = !this.displayElement;
+  }
+}
